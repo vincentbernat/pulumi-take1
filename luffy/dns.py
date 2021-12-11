efea97fe8f561c8ee9389c426056e96f2d14c262
Vincent Bernat <vincent@bernat.ch>
2021-12-11 19:08:12 +0100

    Add DNS zone y.luffy.cx

---
+++ luffy/dns.py
@@ -0,0 +1,9 @@
+import pulumi
+import pulumi_aws as aws
+
+aws.route53.Zone(
+    "y-luffy-cx",
+    comment="DDNS for luffy.cx hosts",
+    name="y.luffy.cx",
+    opts=pulumi.ResourceOptions(protect=True),
+)
