b88b94e8a6d2632b1b0fc8d2df674cc852f50e9a
Vincent Bernat <vincent@bernat.ch>
2021-12-13 20:18:21 +0100

    Tentative to do geolocation
    Unfortunately, we cannot repeat a nameserver...

--- luffy/dns.py
+++ luffy/dns.py
@@ -1,21 +1,23 @@
 import re
+import collections
 import pulumi
 import pulumi_aws as aws
 from .kms import dns_cmk
 
 
-def record(zone, name, rrtype, records, ttl=86400):
+def record(zone, name, rrtype, records, ttl=86400, rname=None, **more):
     if name == "@":
         name = zone._name
     else:
         name = f"{name}.{zone._name}"
     aws.route53.Record(
-        f"{rrtype}-{name}",
+        f"{rrtype}-{name}" if rname is None else rname,
         zone_id=zone.zone_id,
         name=f"{name}",
         type=rrtype,
         ttl=ttl,
         records=records if type(records) is list else [records],
+        **more,
     )
 
 
@@ -23,22 +25,65 @@ def www(zone, name):
     """Create records for web servers."""
     ttl = 60 * 60 * 2
     servers = {
-        "web03": {"A": "95.216.162.158", "AAAA": "2a01:4f9:c010:1a9c::1"},
-        "web04": {"A": "116.203.18.48", "AAAA": "2a01:4f8:1c0c:5eb5::1"},
-        "web05": {"A": "5.161.44.145", "AAAA": "2a01:4ff:f0:b91::1"},
+        "web03": {
+            # Finland
+            "A": "95.216.162.158",
+            "AAAA": "2a01:4f9:c010:1a9c::1",
+            "geolocations": [("country", ["NO", "SE", "FI", "RU", "EE", "LT", "LV"])],
+        },
+        "web04": {
+            # Germany
+            "A": "116.203.18.48",
+            "AAAA": "2a01:4f8:1c0c:5eb5::1",
+            "geolocations": [("continent", ["EU", "AF"])],
+        },
+        "web05": {
+            # US, Virginia
+            "A": "5.161.44.145",
+            "AAAA": "2a01:4ff:f0:b91::1",
+            "geolocations": [("continent", ["NA", "SA"])],
+        },
     }
+    servers = {
+        server: data for server, data in servers.items() if not data.get("disabled")
+    }
+    # Normalize the data a bit
+    geolocations = set()
+    for server, data in servers.items():
+        data["geolocations"] = [(k, v1) for k, v in data["geolocations"] for v1 in v]
+    # Build geolocation sets
+    geolocations = {
+        geoloc for data in servers.values() for geoloc in data["geolocations"]
+    }
+    # Compute records for each location. All servers will be part of
+    # all locations, but we put more weights on the server in the
+    # right location. We don't have enough servers covering all
+    # locations. And of course, web browsers do not support SRV
+    # records.
+    rrs = {}
+    rrs[("country", "*")] = servers.keys()
+    for geoloc in geolocations:
+        rrs[geoloc] = []
+        for server, data in servers.items():
+            nb = 5 if geoloc in data["geolocations"] else 1
+            rrs[geoloc].extend([server] * nb)
+    # Reverse the dictionary
+    records = collections.defaultdict(list)
+    for geoloc, selecteds in rrs.items():
+        records[tuple(selecteds)].append(geoloc)
     for rrtype in ("A", "AAAA"):
-        record(
-            zone,
-            name,
-            rrtype,
-            [
-                data[rrtype]
-                for server, data in servers.items()
-                if not data.get("disabled")
-            ],
-            ttl,
-        )
+        for selecteds, geolocs in records.items():
+            for geoloc in geolocs:
+                record(
+                    zone,
+                    name,
+                    rrtype,
+                    [servers[server][rrtype] for server in selecteds],
+                    ttl=ttl,
+                    rname=f"{rrtype}-{geoloc[0]}-{geoloc[1]}-{name}",
+                    set_identifier=f"geoloc-{geoloc[0]}-{geoloc[1]}",
+                    geolocation_routing_policies=[dict([geoloc])],
+                )
     record(zone, name, "CAA", ['0 issue "buypass.com"', '0 issuewild ";"'])
 
 
