a9c4e9da43a1268f0bd38d2aa9fc11f0e00e9052
Vincent Bernat <vincent@bernat.ch>
2022-01-03 10:49:30 +0100

    nix: convert to poetry2nix

--- flake.nix
+++ flake.nix
@@ -2,13 +2,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    mach-nix = {
-      url = "github:DavHau/mach-nix?ref=3.3.0";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
-    };
     vultr-provider = {
       url = "github:vincentbernat/pulumi-vultr";
       flake = false;
@@ -18,85 +11,93 @@
       flake = false;
     };
   };
-  outputs = { self, nixpkgs, flake-utils, ...}@inputs:
+  outputs = { self, flake-utils, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages."${system}";
-        mach-nix = inputs.mach-nix.lib."${system}";
-        # Grab versions we got to match for Python
-        pulumi-version = pkgs.pulumi-bin.version;
-        pulumi-XXX-version = what: builtins.elemAt
-          (pkgs.lib.flatten
-            (builtins.filter (x: x != null)
-              (map
-                (x: builtins.match "^pulumi-resource-${what}-v([0-9.]+)-linux.*" x.name)
-                pkgs.pulumi-bin.srcs)))
-          0;
-        pulumi-aws-version = pulumi-XXX-version "aws";
-        pulumi-hcloud-version = pulumi-XXX-version "hcloud";
+        pkgs = inputs.nixpkgs.legacyPackages."${system}";
+        lib = pkgs.lib;
+        poetry = pkgs.poetry2nix.mkPoetryPackages {
+          projectDir = ./.;
+          overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
+            doCheck = false;
+            doInstallCheck = false;
+            dontUseSetuptoolsCheck = true;
+            dontUsePytestCheck = true;
+          });
+        };
         # Custom providers (pulumi+python)
         pulumi-providers =
           let builder = name: src': args: {
-                plugin = pkgs.buildGoModule (rec {
-                  pname = "pulumi-provider-${name}";
-                  version = "0.0.0";
-                  src = src';
-                  modRoot = "./provider";
-                  preBuild = ''
+            plugin = pkgs.buildGoModule (rec {
+              pname = "pulumi-provider-${name}";
+              version = "0.0.0";
+              src = src';
+              modRoot = "./provider";
+              preBuild = ''
                 cat <<EOF > pkg/version/version.go
                 package version
                 var Version string = "v0.0.0"
                 EOF
               '';
-                  subPackages = [ "cmd/pulumi-resource-${name}" ];
-                } // args);
-                python = mach-nix.buildPythonPackage {
-                  pname = "pulumi_${name}";
-                  src = "${src'}/sdk/python";
-                  requirementsExtra = "pulumi==${pulumi-version}";
-                };
-              };
-          in
-            {
-              vultr = builder "vultr" inputs.vultr-provider {
-                vendorSha256 = "sha256-EkSZ2pGlyBLz+FL/0ViXmzKmWjcYqYYJ+rY18LF3Q4E=";
-              };
-              gandi = builder "gandi" inputs.gandi-provider {
-                vendorSha256 = "sha256-LjUxilWiVyzqjhRSfJ+tnxkj3JWb1o7xs1zBns1cTHA=";
-              };
+              subPackages = [ "cmd/pulumi-resource-${name}" ];
+            } // args);
+            python = pkgs.python3.pkgs.buildPythonPackage {
+              pname = "pulumi_${name}";
+              version = "0.0.0";
+              src = "${src'}/sdk/python";
+              doCheck = false;
+              propagatedBuildInputs = poetry.poetryPackages;
             };
+          };
+          in
+          {
+            vultr = builder "vultr" inputs.vultr-provider {
+              vendorSha256 = "sha256-EkSZ2pGlyBLz+FL/0ViXmzKmWjcYqYYJ+rY18LF3Q4E=";
+            };
+            gandi = builder "gandi" inputs.gandi-provider {
+              vendorSha256 = "sha256-LjUxilWiVyzqjhRSfJ+tnxkj3JWb1o7xs1zBns1cTHA=";
+            };
+          };
+        # Check Python versions for mismatch
+        pulumi-bin-versions = builtins.listToAttrs (map
+          (p:
+            let
+              match = builtins.match "^(pulumi.*)-v([0-9.]+)-.*" p.name;
+            in
+            {
+              name = builtins.replaceStrings [ "-resource-" ] [ "-" ] (lib.head match);
+              value = lib.last match;
+            }
+          )
+          pkgs.pulumi-bin.srcs);
+        poetry-versions = lib.filterAttrs
+          (n: _: (builtins.match "^pulumi.*" n) != null)
+          (builtins.listToAttrs (map
+            (p: { name = p.pname; value = p.version; })
+            poetry.poetryPackages));
+        warnWhenMismatch = builtins.attrValues (lib.mapAttrs
+          (p: v:
+            let bv = pulumi-bin-versions."${p}"; in
+            lib.warnIf (bv != v) "Versions mismatch for ${p}: ${bv} != ${v}" null)
+          poetry-versions);
         # Python environment
-        python-env = mach-nix.mkPython {
-          requirements = ''
-            pulumi==${pulumi-version}
-            pulumi-aws==${pulumi-aws-version}
-            pulumi-hcloud==${pulumi-hcloud-version}
-
-            # Needed for pulumi to detect providers
-            pip
-            setuptools
-
-            # Other tools
-            black
-          '';
-          packagesExtra = [
+        python-env = builtins.deepSeq warnWhenMismatch
+          (poetry.python.withPackages (ps: poetry.poetryPackages ++ [
+            ps.pip
+            ps.setuptools
+            ps.black
             pulumi-providers.vultr.python
             pulumi-providers.gandi.python
-          ];
-          providers = {
-            pip = "nixpkgs";
-            setuptools = "nixpkgs";
-            black = "nixpkgs";
-          };
-        };
-      in {
-        devShell = pkgs.mkShell {
+          ]));
+      in
+      {
+        packages.poetry = pkgs.poetry;
+        devShell = python-env.env.overrideAttrs (oldAttrs: {
           name = "pulumi-take1";
           buildInputs = [
             pkgs.pulumi-bin
             pulumi-providers.vultr.plugin
             pulumi-providers.gandi.plugin
-            python-env
           ];
           shellHook = ''
             export PULUMI_SKIP_UPDATE_CHECK=1
@@ -109,6 +110,6 @@
                 eval $(pass show personal/$(echo $p | tr 'A-Za-z' 'N-ZA-Mn-za-m') | grep '^export')
             done
           '';
-        };
-    });
+        });
+      });
 }
